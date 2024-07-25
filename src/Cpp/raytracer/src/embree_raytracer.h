3ab8e0c6808783f2fc12f1e889e5a96a48f72990
apric0ts <austinhwang18@gmail.com>
2024-07-25 14:58:48 -0400

    Removed extra qualification in class definition for Vector3D

--- src/Cpp/raytracer/src/embree_raytracer.h
+++ src/Cpp/raytracer/src/embree_raytracer.h
@@ -39,7 +39,7 @@ namespace HF::RayTracer {
 	struct Vector3D {
 		double x; double y; double z;
 
-		inline Vector3D::Vector3D(double x, double y, double z) {
+		inline Vector3D(double x, double y, double z) {
 			this->x = x;
 			this->y = y;
 			this->z = z;
